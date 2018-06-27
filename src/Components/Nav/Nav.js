b2cfa1d72cf81d3bf8c4ec218a1ed4cd78329043
sbowen88 <sbowey@gmail.com>
2018-06-27 17:05:00 -0600

    navbar and carousel almost done

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,6 +4,13 @@ import logo from "./../../Assets/Screen Shot 2018-06-27 at 9.21.26 AM.png";
 import "./Nav.css";
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    };
+  }
+
   render() {
     return (
       <div className="nav-root">
@@ -11,22 +18,36 @@ class Nav extends Component {
           <Link to="/">
             <img className="logo" src={logo} alt="" />
           </Link>
-          <Link className="nav-link" to="/popular/movies">
+          <div className = 'dropdown'>
+          <Link className="nav-link link-1" to="/popular/movies">
             Movies
           </Link>
-          <Link className="nav-link" to="/popular/people">
+          <div className="dropdown-content">
+            <a href="#">Popular</a>
+            <a href="#">Top Rated</a>
+            <a href="#">Upcoming</a>
+            <a href="#">Now Playing</a>
+          </div>
+          </div>
+          <div className="dropdown">
+          <Link className="nav-link" id='link-1' to="/popular/people">
             People
           </Link>
+            <div class="dropdown-content">
+              <a href="#">Popular People</a>
+            </div>
+          </div>
         </div>
         <div className="nav-right">
           <Link className="nav-link" to="/popular/people">
             Login
           </Link>
-          <Link className="nav-link" to="/popular/people">
-            Sign Up
-          </Link>
+          {this.state.loggedIn === false ? null : (
+            <Link className="nav-link" to="/popular/people">
+              My Profile
+            </Link>
+          )}
         </div>
-
       </div>
     );
   }
