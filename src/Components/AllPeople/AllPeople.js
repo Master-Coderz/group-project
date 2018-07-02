bda5c8dbfb25133e943562659f2e62e5d852737f
sbowen88 <sbowey@gmail.com>
2018-07-02 10:30:29 -0600

    default image on error

--- src/Components/AllPeople/AllPeople.js
+++ src/Components/AllPeople/AllPeople.js
@@ -36,7 +36,7 @@ export default class AllPeople extends Component {
               width="185px"
               height="278px"
               alt=''
-              onerror="this.onerror=null;this.src='http://futureuniversity.com/wp-content/themes/envision/lib/images/default-placeholder-700x934.png';"
+              onError={(e)=>{e.target.src="http://futureuniversity.com/wp-content/themes/envision/lib/images/default-placeholder-700x934.png"}}
               />
           </Link>
           <h3 className = 'all_people_name'>{e.name}</h3>
