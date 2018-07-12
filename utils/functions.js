23d0e23e1c7cfa87dd679846a73040890e5828e5
CarterChilds <carterchilds@live.com>
2018-07-12 16:45:37 -0600

    merging

--- utils/functions.js
+++ utils/functions.js
@@ -1,36 +1,42 @@
 const axios = require('axios')
 
-module.exports = {
-    getPopular:   async() => {
-        await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=7e09033ae18d3f476763f2bf4ee67f60&language=en-US&page=1`
-        ).then((res) => {
-        return res
-          });
-      },
+const fns = {
+  getPopular: ()  => 
+    axios
+    .get('https://api.themoviedb.org/3/movie/299536?api_key=7e09033ae18d3f476763f2bf4ee67f60&language=en-US')
+    .then(res => res.data)
+    .catch(err => 'error')
+  ,
 
-      getInTheaters: async ()  => {
-         await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
-        ).then((res) => {
-          this.setState({
-            inTheaters: res.data.results
-          });
-        });
-      },
+  getMovie: ()  => {
+return axios
+    .get('https://api.themoviedb.org/3/movie/299536?api_key=7e09033ae18d3f476763f2bf4ee67f60&language=en-US')
+    .then(res => res.data)
+    .catch(err => 'error')
+  },
 
-      getWatchlist: () => {
-        axios.get('/api/getWatchlist')
-          .then((res) => {
-            this.setState({
-              watchlist: res.data
-            })
-            let onList = this.state.watchlist.filter(e => {
-              return e.id === this.props.match.params.id
-    
-            })
-            if (onList.length > 0) {
-              this.setState({ onWatchList: true })
-            }
-    
-          })
-      }
-}
+  getUser: (num) => {
+   const user = {
+      firstName: 'Carter',
+      lastName: "Childs",
+      age: num,
+    }
+    return user  
+  },
+  fetchUser: () =>
+  axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error'),
+
+  fetchUser: () =>
+   axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
+
+};
+
+  
+
+module.exports = fns
